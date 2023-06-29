3479fe26a8d94f8c6f4dff44680f489fdb89a287
MaMwWun <137387176+MaMwWun@users.noreply.github.com>
2023-06-29 19:33:35 +0800

    Update utils.py
    coingecko api rate limits change, sleep goes up

--- MEV Script (Weintraub et al.)/data-collection/mev/utils/utils.py
+++ MEV Script (Weintraub et al.)/data-collection/mev/utils/utils.py
@@ -1,19 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import json
 import os
 import time
-import json
+
 import requests
-
 from web3 import Web3
-from .settings import UPDATE_PRICES
 
-class colors:
-    INFO = '\033[94m'
-    OK = '\033[92m'
-    FAIL = '\033[91m'
-    END = '\033[0m'
 
 def get_coin_list():
     print("Getting list of coins from CoinGecko.com...")
@@ -24,44 +15,49 @@ def get_coin_list():
             coin_list[Web3.toChecksumAddress(coin["platforms"]["ethereum"].lower())] = coin["id"]
     return coin_list
 
+
 def get_prices():
     coin_list = get_coin_list()
     print("Fetching latest prices from CoinGecko.com...")
-    from_timestamp = str(1392577232) # Sun Feb 16 2014 19:00:32 GMT+0000
+    from_timestamp = str(1392577232)  # Sun Feb 16 2014 19:00:32 GMT+0000
     to_timestamp = str(int(time.time()))
     prices = dict()
-    path = os.path.dirname(__file__)
-    if os.path.exists(path+"/prices.json"):
-        with open(path+"/prices.json", "r") as f:
+    if os.path.exists("prices.json"):
+        with open("prices.json", "r") as f:
             prices = json.load(f)
     else:
-        prices["eth_to_usd"] = requests.get("https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from="+from_timestamp+"&to="+to_timestamp).json()["prices"]
-    if UPDATE_PRICES:
-        for address in coin_list:
-            if address not in prices:
-                market_id = coin_list[address]
-                print(address, market_id)
-                try:
-                    reponse = requests.get("https://api.coingecko.com/api/v3/coins/"+market_id+"/market_chart/range?vs_currency=eth&from="+from_timestamp+"&to="+to_timestamp)
-                    prices[address] = reponse.json()["prices"]
-                    time.sleep(1)
-                except Exception as e:
-                    print(e+":", reponse.text)
-                    with open("prices.json", "w") as f:
-                        json.dump(prices, f, indent=2)
-                    return prices
-        with open("prices.json", "w") as f:
-            json.dump(prices, f, indent=2)
+        prices["eth_to_usd"] = requests.get(
+            "https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=" + from_timestamp + "&to=" + to_timestamp).json()["prices"]
+
+    for address in coin_list:
+        if address not in prices:
+            market_id = coin_list[address]
+            print(address, market_id)
+            try:
+                response = requests.get(
+                    "https://api.coingecko.com/api/v3/coins/" + market_id + "/market_chart/range?vs_currency=eth&from=" + from_timestamp + "&to=" + to_timestamp)
+                prices[address] = response.json()["prices"]
+                time.sleep(20)  # wait for 10 seconds before making the next API call
+            except Exception as e:
+                print(str(e) + ":", response.text)
+                with open("prices.json", "w") as f:
+                    json.dump(prices, f, indent=2)
+                continue  # skip to the next coin in case of an error
+
+    with open("prices.json", "w") as f:
+        json.dump(prices, f, indent=2)
+
     print("Fetched prices for", len(prices), "coins.")
     return prices
 
-def get_price_from_timestamp(timestamp, prices):
-    timestamp *= 1000
-    one_eth_to_usd = prices[-1][1]
-    for index, _ in enumerate(prices):
-        if index < len(prices)-1:
-            if prices[index][0] <= timestamp and timestamp <= prices[index+1][0]:
-                return prices[index][1]
-    print(colors.FAIL+"Error: Could not find timestamp. Returning latest price instead."+colors.END)
-    print(colors.FAIL+"Please consider updating prices.json!"+colors.END)
-    return one_eth_to_usd
+
+if __name__ == '__main__':
+    while True:
+        try:
+            get_prices()
+            break  # exit the loop if all coins have been fetched successfully
+        except Exception as e:
+            print("Error occurred:", str(e))
+            time.sleep(60)  # wait for 60 seconds before retrying
+            continue  # retry fetching prices for all coins
+
